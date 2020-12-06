f6c3d53f86a91ab3b13f2efe4538e4b8ce68e210
Jacob Perez <jacobxperez@gmx.com>
2020-12-06 00:33:10 -0700

    Updated dropdown

--- js/script.js
+++ js/script.js
@@ -90,84 +90,59 @@ document.addEventListener("DOMContentLoaded", function() {
 
     }); // end Slider
 
-    // Accordion v1.0
-    (function () {
-        const getAccordion = document.querySelectorAll(".js-accordion");
+    // Toggle
+   (function () {
+       const getToggle = document.querySelectorAll(".js-toggle");
+       const getToggleReset = document.querySelectorAll(".js-toggle-reset");
 
-        for (let i = 0; i < getAccordion.length; i++) {
+       for (let i = 0; i < getToggle.length; i++) {
+           getToggle[i].addEventListener("click", function(e) {
 
-            getAccordion[i].addEventListener("click", function(e) {
+               // toggle class show
+               if (this.classList.contains("active") === false) {
 
-                // toggle class show
-                if (this.classList.contains("accordion-active") === false) {
+                   this.classList.add("active");
 
-                    this.classList.add("accordion-active");
+               } else if (this.classList.contains("active") === true) {
 
-                } else if (this.classList.contains("accordion-active") === true) {
+                   this.classList.remove("active");
 
-                    this.classList.remove("accordion-active");
+               } // end if block
 
-                } // end if block
+               e.stopPropagation();
+           });
+       } // end for loop
 
-                e.stopPropagation();
-            });
-        } // end for loop
-    })();
-    // end Accordion
-
-
-    // Dropdown v1.0
-    (function () {
-        const getDropdown = document.querySelectorAll(".js-dropdown");
-
-        for (let i = 0; i < getDropdown.length; i++) {
-            getDropdown[i].addEventListener("click", function(e) {
-
-                // toggle class show
-                if (this.classList.contains("dropdown-active") === false) {
-
-                    this.classList.add("dropdown-active");
-
-                } else if (this.classList.contains("dropdown-active") === true) {
-
-                    this.classList.remove("dropdown-active");
-
-                } // end if block
-
-                e.stopPropagation();
-            });
-        } // end for loop
-
-        // Hide dropdown on window click
-        document.addEventListener("click", function(e) {
-            // check if target is not dropdown
-            if (e.target !== getDropdown) {
-                for (let i = 0; i < getDropdown.length; i++) {
-                    // removes class show from all dropdowns
-                    getDropdown[i].classList.remove("dropdown-active");
-                } // end for loop
-            }
-        });
-    })();
-    // end Dropdown
+       // Hide ghost-box on window click
+       document.addEventListener("click", function(e) {
+           // check if target is not dropdown
+           if (e.target !== getToggleReset) {
+               for (let i = 0; i < getToggleReset.length; i++) {
+                   // removes class show from all dropdowns
+                   getToggleReset[i].classList.remove("active");
+               } // end for loop
+           }
+       });
+   })();
+   // end Toggle
 
 
-    // Smooth Scroll v1.0
-    (function () {
-        const intLinks = document.querySelectorAll("a[href^='#']");
+   // Smooth Scroll
+   (function () {
+       const intLinks = document.querySelectorAll("a[href^='#']");
 
-        for (let i = 0; i < intLinks.length; i++) {
-            intLinks[i].addEventListener("click", function(e) {
+       for (let i = 0; i < intLinks.length; i++) {
+           intLinks[i].addEventListener("click", function(e) {
 
-                e.preventDefault();
+               e.preventDefault();
 
-                document.querySelector(this.getAttribute("href")).scrollIntoView({
-                    behavior: "smooth"
-                });
+               document.querySelector(this.getAttribute("href")).scrollIntoView({
+                   behavior: "smooth"
+               });
 
-            });
-        } // end for loop
-    })();
-    // end Smooth Scroll
+           });
+       } // end for loop
+   })();
+   // end Smooth Scroll
 
 }); // end Script
