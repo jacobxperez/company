b10af2aceccfd787f6ef05795ca38ea10abae367
Jacob <jacobxperez@gmx.com>
2023-03-03 22:44:51 -0700

    Update

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -1,24 +1,29 @@
 const toggle = () => {
-    const getToggle = document.querySelectorAll("[data-toggle]")
+    const getToggle = document.querySelectorAll('[data-toggle]')
 
     // toggles attribute
     function toggleAttr(item) {
-        item.addEventListener("click", (e) => {
-            !item.hasAttribute("data-state", "active") ? item.setAttribute("data-state", "active") : item.removeAttribute("data-state")
+        item.addEventListener('click', (e) => {
+            item.hasAttribute('data-state', 'active')
+                ? item.removeAttribute('data-state')
+                : item.setAttribute('data-state', 'active')
             e.stopPropagation()
         })
     }
 
     getToggle.forEach(toggleAttr)
 
-    function removeAtt(item, match, e) {
-        if (e.target !== item && item.matches(match)) {
-            item.removeAttribute("data-state")
+    function removeAtt(item, e) {
+        if (
+            (e.target !== item && item.matches('[data-toggle~="pop"]')) ||
+            item.matches('[data-toggle~="tooltip"]')
+        ) {
+            item.removeAttribute('data-state')
         }
     }
 
-    document.addEventListener("click", (e) => {
-        getToggle.forEach((item) => removeAtt(item, '[data-toggle~="dropdown"]', e))
+    document.addEventListener('click', (e) => {
+        getToggle.forEach((item) => removeAtt(item, e))
     })
 }
 
